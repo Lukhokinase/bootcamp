548e2b12824c0d4d9e7d6aa90ee3bc43f2539d4c
Lukhokinase <lukhokinase@gmail.com>
2023-04-04 14:43:45 +0200

    JS arrays

---
+++ javascript-basics/jsbasics.js
@@ -0,0 +1,25 @@
+// function addition(a,b){
+//     return a + b 
+// }
+// console.log(addition(5,6))
+
+// let a=1000
+// let b="20"
+// if (a>b){
+//     console.log("yes a is greater then b")
+// }else{
+//     console.log("")
+// }
+
+let arr1 = [2,4,5,35,435,6,43,2,34,5];
+
+// for (let x = 0; x < arr1.length; x++) {
+//     if(arr1[x]% 5 === 0){
+//         console.log(arr1[x])
+//     }
+    
+// }
+
+arr1.push(380);
+arr1.unshift("lukho");
+console.log(arr1)
