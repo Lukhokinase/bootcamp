e44cf1e270d0f23a52610470450ded788aed6dbc
Lukhokinase <lukhokinase@gmail.com>
2023-04-21 17:00:21 +0200

    first commit

--- calcutor/bbb.js
+++ calcutor/bbb.js
@@ -1,11 +1,11 @@
 let numb = [70,40,36,72,81,89,18,62,73,45,59]
  let snum = 0
- for(let k=0; k > numb.length; k++ ){
+ for(k=0; k > numb.length; k++ ){
     snum += numb[k];
   
  }
 
- let aver = snum/numb.length
+ let aver = snum
 
- console.log("average is " + aver)
+ console.log(aver)
 
